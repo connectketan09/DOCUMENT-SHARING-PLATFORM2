f29a9a0d1b39cbacf2e7ab19db03897a97b976af
connectketan09 <connect.ketan09@gmail.com>
2026-03-21 12:27:06 +0530

    Add names to URL patterns for better clarity

--- shareapp/urls.py
+++ shareapp/urls.py
@@ -3,9 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('upload/', views.upload_file),
-    path('processing/', views.processing),
-    path('result/', views.qr_result),
-    path('download/<str:filename>/', views.download_file),
+    path('', views.home, name='home'),
+
+    # 👇 this fixes your issue
+    path('account/', views.account, name='account'),
+
+    path('upload/', views.upload_file, name='upload'),
+    path('processing/', views.processing, name='processing'),
+    path('result/', views.qr_result, name='result'),
+    path('download/<str:filename>/', views.download_file, name='download'),
 ]
